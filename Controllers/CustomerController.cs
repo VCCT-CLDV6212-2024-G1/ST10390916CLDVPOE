9fe319fa68d67cf6534fc682c15b583b6ff066a0
WeylinV <st10390916@vcconnect.edu.za>
2024-11-11 22:43:08 +0200

    Added sql database tasks.

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -5,41 +5,25 @@ namespace ST10390916CLDVPOE.Controllers
 {
     public class CustomerController : Controller
     {
-
-        private readonly TableService _tableService;
-        private readonly FileService _fileService;
-
-        public CustomerController(TableService tableService, FileService fileService)
-        {
-            _tableService = tableService;
-            _fileService = fileService;
-        }
-
         public IActionResult Customer()
         {
             return View();
         }
 
-        //add customer to customer table
+        //add customer to sql database table
         [HttpPost]
         public async Task<IActionResult> AddCustomer(Customer customer)
         {
-            if (ModelState.IsValid)
-            {
-                await _tableService.AddEntityAsync(customer);
-            }
+            customer.InsertCustomer(customer);
+            
             return RedirectToAction("Customer");
         }
 
+        //poe part 2 code that is deleted
         //upload contract to file share
         [HttpPost]
-        public async Task<IActionResult> UploadContract(IFormFile file)
+        public async Task<IActionResult> UploadContract()
         {
-            if (file != null)
-            {
-                using var stream = file.OpenReadStream();
-                await _fileService.UploadFileAsync("contracts", file.FileName, stream);
-            }
             return RedirectToAction("Customer");
         }
 
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,18 +5,6 @@ namespace ST10390916CLDVPOE.Controllers
 {
     public class ProductController : Controller
     {
-
-        private readonly BlobService _blobService;
-        private readonly TableService _tableService;
-        private readonly QueueService _queueService;
-
-        public ProductController(BlobService blobService, TableService tableService, QueueService queueService)
-        {
-            _blobService = blobService;
-            _tableService = tableService;
-            _queueService = queueService;
-        }
-
         public IActionResult Products()
         {
             return View();
@@ -31,30 +19,26 @@ namespace ST10390916CLDVPOE.Controllers
         [HttpPost]
         public async Task<IActionResult> AddProduct(Product product) //async task can run in background
         {
-            if (ModelState.IsValid)
-            {
-                await _tableService.AddEntityAsync(product); ///await - UI stays responsive
-            }
+            product.InsertProduct(product);
+
             return RedirectToAction("Products");
         }
 
+        //add new order to sql database
+        [HttpPost]
+        public async Task<IActionResult> ProcessOrder(Order order)
+        {
+            order.Message = $"Order {order.OrderID} was processed";
+            order.InsertOrder(order);
+
+            return RedirectToAction("Products");
+        }
+
+        //poe part 2 code that is deleted
         //add product image to blob
         [HttpPost]
-        public async Task<IActionResult> UploadImage(IFormFile file)
+        public async Task<IActionResult> UploadImage()
         {
-            if (file != null)
-            {
-                using var stream = file.OpenReadStream();
-                await _blobService.UploadBlobAsync("images", file.FileName, stream);
-            }
-            return RedirectToAction("Products");
-        }
-
-        //send string to Azure Queue
-        [HttpPost]
-        public async Task<IActionResult> ProcessOrder(string orderId)
-        {
-            await _queueService.SendMessageAsync("order-processing", $"Processing order {orderId}");
             return RedirectToAction("Products");
         }
 
